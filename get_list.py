c02443c9f5b958935a7ba2a381d174c832bb4a26
MichaelMart <40335523+MichaelMart@users.noreply.github.com>
2022-07-08 18:42:59 +0300

    Update get_list.py

--- get_list.py
+++ get_list.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 list_symbols = []
-API = 'https://api.cryptorank.io/v1/currencies?limit=5300&api_key=42277916e311b55400c2a298eadd6ab6353a256ba9a24830bb9fcc15b2f3'
+API = ' '
 r = requests.get(API).json()
 for i in range(0, 5300):
     list_symbols.append(r.get('data')[i].get('slug'))
